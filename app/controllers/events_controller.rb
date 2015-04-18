bf2b0ed89a2654ffb5ff3fae8f03c148cb148938
superlou <lousimons@gmail.com>
2015-04-18 19:44:44 -0400

    Basic CRUD

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,4 +11,27 @@ class EventsController < ApplicationController
   def show
     render json: Event.find(params[:id])
   end
+
+  def create
+    event = Event.create!(event_params)
+    render json: event
+  end
+
+  def update
+    event = Event.find(params[:id])
+    event.update_attributes(event_params)
+    render json: event
+  end
+
+  def destroy
+    event = Event.find(params[:id])
+    event.destroy
+    render json: {}, status: 200
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :con_id)
+  end
 end
