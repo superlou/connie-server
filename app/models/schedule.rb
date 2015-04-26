09bfd0cab5c6117a3d3d55c4e700a2536373b5c8
superlou <lousimons@gmail.com>
2015-04-26 12:29:05 -0400

    Send events with schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,4 +2,5 @@ class Schedule < ActiveRecord::Base
   has_many :schedule_places
   has_many :places, through: :schedule_places
   belongs_to :con
+  has_many :events, through: :con
 end
--- app/serializers/schedule_serializer.rb
+++ app/serializers/schedule_serializer.rb
@@ -2,4 +2,5 @@ class ScheduleSerializer < ActiveModel::Serializer
   embed :ids, include: true
   attributes :id, :start, :finish, :name
   has_many :places
+  has_many :events
 end
